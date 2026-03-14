c0be28175a472b615bce5aad363b1c6bbdfdd96b
kalanaashen <northernwolf1472@gmail.com>
2026-03-14 21:46:51 +0530

    create interface for task form

---
+++ frontend/my-react-app/src/components/Form.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+export const Form = () => {
+  return (
+    <div>
+      <div className="flex flex-col gap-4 pt-10 items-center justify-center  ">
+        <div className="flex flex-row gap-x-2 ">
+          <label className="text-2xl font-medium text-pink-600">Task</label>
+          <input
+            type="text"
+            className="border-2 border-pink-300 rounded-xl w-2xl p-2.5 focus:outline-none focus:ring-2 focus:ring-pink-300"
+          />
+        </div>
+        <div className="flex flex-row gap-x-2">
+          <label className="text-2xl font-medium text-pink-600">
+            Description
+          </label>
+          <textarea
+            cols={50}
+            rows={10}
+            className=" border-2 border-pink-300 rounded-3xl p-2.5 focus:outline-none focus:ring-2 focus:ring-pink-300"
+          />
+        </div>
+        <div className="flex flex-row gap-x-2 ">
+          <label className="text-2xl font-medium text-pink-600">
+            Is it Complete
+          </label>
+          <input
+            type="checkbox"
+            className="border border-pink-300 rounded-2xl size-10 focus:outline-none focus:ring-2 focus:ring-pink-300"
+          />
+        </div>
+
+        <div>
+          <button className="bg-pink-500 text-white font-bold py-4 px-6 rounded-full hover:bg-pink-700 focus:outline-none focus:ring-2 focus:ring-pink-300 focus:ring-offset-2">
+            Add Task
+          </button>
+          <button className="bg-gray-300 text-gray-700 font-bold py-4 px-6 rounded-full hover:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-300 focus:ring-offset-2 ml-4">
+            Clear
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
