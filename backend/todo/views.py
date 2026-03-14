244745b394b83e79dfda9d4ae449be0498bcff9d
kalanaashen <northernwolf1472@gmail.com>
2026-03-14 20:03:15 +0530

    add routers add serilaers add viewsets

--- backend/todo/views.py
+++ backend/todo/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets,filters
+from .models import TODO
+from .serializers import TODOSerializer
+from django_filters.rest_framework import DjangoFilterBackend
 # Create your views here.
+class TODOViewset(viewsets.ModelViewSet):
+  
+  
+    queryset=TODO.objects.all()
+    serializer_class=TODOSerializer
+    filter_backends=[DjangoFilterBackend,filters.OrderingFilter,filters.SearchFilter]
+    
+    filterset_fields=['title','user','is_completed']
+    ordering_fields=['is_completed','created_at','updated_at']
+    search_fields=['title']
+    
+    
+    
